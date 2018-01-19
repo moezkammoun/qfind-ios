8990b77e73d658a51aa1b45f65b90e13611477b5
Vidya R <vidyar851@gmail.com>
2018-01-19 10:20:33 +0530

    [#QFIN-79] History Page UI

---
+++ QFind/ReusableComponents/HistoryOrSearchCell/SectionHeaderCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  SectionHeaderCollectionReusableView.swift
+//  QFind
+//
+//  Created by Exalture on 18/01/18.
+//  Copyright © 2018 QFind. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeaderCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var headerLabel: UILabel!
+    
+}
