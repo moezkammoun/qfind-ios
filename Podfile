397b7fa97d31d4354a76b5604be8515b18c4e6c8
Vidya R <vidyar851@gmail.com>
2018-01-24 20:39:17 +0530

    [QFIN-77]Added Alamofire

---
+++ Podfile
@@ -0,0 +1,7 @@
+
+platform :ios, '9.0'
+use_frameworks!
+target 'QFind' do
+ pod 'Alamofire'
+
+end
---
+++ Pods/Target Support Files/Pods-QFind/Pods-QFind-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QFind : NSObject
+@end
+@implementation PodsDummy_Pods_QFind
+@end
